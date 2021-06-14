36268c7ee00b18a80c29a4c06154ec8df32044f4
Shubham Rane <official.shubhamrane@gmail.com>
2021-06-14 10:55:37 -0400

    Fetching vehicles details array

--- Trucker_Demo/src/main/java/com/example/trucker_demo/controller/TruckerDemo_Controller.java
+++ Trucker_Demo/src/main/java/com/example/trucker_demo/controller/TruckerDemo_Controller.java
@@ -5,31 +5,46 @@ import com.example.trucker_demo.model.VehicleReading_Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 public class TruckerDemo_Controller {
 
-    @Autowired
-    VehicleDetails_Model vehicleDetails_model;
 
-    @GetMapping("/vehicle")
-    @ResponseBody
-    public VehicleDetails_Model getVehicleDetails(){
-        vehicleDetails_model.setVin("7dg38fhf62ifj");
-        vehicleDetails_model.setModel("Honda Accord");
-        vehicleDetails_model.setMake("2019");
-        vehicleDetails_model.setMaxFuelVolumn(15);
-
-        System.out.println(vehicleDetails_model);
-        return vehicleDetails_model;
-    }
+//    @GetMapping("/vehicle")
+//    @ResponseBody
+//    public VehicleDetails_Model getVehicleDetails(){
+//        VehicleDetails_Model vehicleDetails_model = new VehicleDetails_Model();
+//        vehicleDetails_model.setVin("7dg38fhf62ifj");
+//        vehicleDetails_model.setModel("Honda Accord");
+//        vehicleDetails_model.setMake("2019");
+//        vehicleDetails_model.setMaxFuelVolumn(15);
+//
+//        System.out.println(vehicleDetails_model);
+//        return vehicleDetails_model;
+//    }
 
     @PostMapping("/readings")
     @ResponseBody
     public VehicleReading_Model postVehicleReadings(
-            @RequestBody VehicleReading_Model vehicleReading_model
-    ){
+            @RequestBody VehicleReading_Model vehicleReading_model){
+
         System.out.println(vehicleReading_model);
         return  vehicleReading_model;
+    }
+
+    /** under construction */
+    @PutMapping("/vehicles")
+    @ResponseBody
+    public VehicleDetails_Model[] postVehicleDetails(
+            @RequestBody VehicleDetails_Model[] vehicleDetails_model){
+
+        System.out.println(Arrays.toString(vehicleDetails_model));
+        return vehicleDetails_model;
+
     }
 
 }
