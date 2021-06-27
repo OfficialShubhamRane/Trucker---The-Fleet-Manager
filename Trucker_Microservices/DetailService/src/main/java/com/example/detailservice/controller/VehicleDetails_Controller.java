836bc80030f455e1e9c20084734726aaf60ccf89
Shubham Rane <official.shubhamrane@gmail.com>
2021-06-27 19:20:02 -0400

    Vehicle details microservice done

--- Trucker_Microservices/DetailService/src/main/java/com/example/detailservice/controller/VehicleDetails_Controller.java
+++ Trucker_Microservices/DetailService/src/main/java/com/example/detailservice/controller/VehicleDetails_Controller.java
@@ -5,14 +5,25 @@ import com.example.detailservice.service.VehicleDetails_Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class VehicleDetails_Controller {
 
     @Autowired
     VehicleDetails_Service vehicleDetails_service;
 
+    /** Accepts vehicle details array */
     @PutMapping("/vehicles")
     public void putVehicleDetails(@RequestBody  VehicleDetails_Model[] vehicleDetails_models){
         vehicleDetails_service.saveVehicleDetails(vehicleDetails_models);
+    }
+
+    /** returns vehicle details */
+    @GetMapping({"api/vehicles/","api/vehicles/{vin}"})
+    @ResponseBody
+    public List<VehicleDetails_Model> getVehicleDetails(@PathVariable(required = false) String vin){
+        System.out.println("vin: " + vin);
+        return vehicleDetails_service.getVehicleDetails(vin);
     }
 }
--- Trucker_Microservices/DetailService/src/main/java/com/example/detailservice/service/VehicleDetails_Service.java
+++ Trucker_Microservices/DetailService/src/main/java/com/example/detailservice/service/VehicleDetails_Service.java
@@ -5,12 +5,16 @@ import com.example.detailservice.repository.VehicleDetails_Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class VehicleDetails_Service {
 
     @Autowired
     VehicleDetails_Repository vehicleDetails_repository;
 
+    /** saves all vehicle details */
     public void saveVehicleDetails(VehicleDetails_Model[] vehicleDetails_models) {
 
         for (VehicleDetails_Model vehicleDetails : vehicleDetails_models) {
@@ -18,5 +22,20 @@ public class VehicleDetails_Service {
             vehicleDetails_repository.save(vehicleDetails);
         }
 
+    }
+
+    /** returns vehicle details */
+    public List<VehicleDetails_Model> getVehicleDetails(String vin) {
+
+        List<VehicleDetails_Model> vehicleDetails_list = new ArrayList<>();
+        if(vin == null){
+            vehicleDetails_list = vehicleDetails_repository.findAll();
+        }else if( vehicleDetails_repository.existsById(vin) ){
+            vehicleDetails_list.add(vehicleDetails_repository.findByVin(vin));
+        }else{
+            System.out.println("No details exists");
+        }
+
+        return vehicleDetails_list;
     }
 }
